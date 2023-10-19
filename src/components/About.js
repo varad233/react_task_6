02e575ebc3543c2d9abc0d993171b7bc10bd2a46
varad233 <136105601+varad233@users.noreply.github.com>
2023-10-19 20:34:48 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+        SOMETHING ABOUT ME 
+      
+      </p>
+      
+    </section>
+  );
+};
+
+export default About;
